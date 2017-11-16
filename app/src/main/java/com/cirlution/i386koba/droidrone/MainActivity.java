3f9b857b828cb2b09f6a837ace2bcfc628652cb9
i386koba <i386koba@gmail.com>
2017-11-16 11:30:59 +0900

    UserRecoverableAuthIOException が発生した際の処理を追加

--- app/src/main/java/com/cirlution/i386koba/droidrone/MainActivity.java
+++ app/src/main/java/com/cirlution/i386koba/droidrone/MainActivity.java
@@ -20,6 +20,7 @@ package com.cirlution.i386koba.droidrone;
         import android.hardware.SensorManager;
         import android.location.Location;
         import android.location.LocationManager;
+        import android.os.AsyncTask;
         import android.os.Build;
         import android.os.Bundle;
         import android.support.annotation.NonNull;
@@ -40,15 +41,21 @@ package com.cirlution.i386koba.droidrone;
         import com.google.android.gms.location.LocationServices;
         import com.google.api.client.extensions.android.http.AndroidHttp;
         import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+        import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+        import com.google.api.client.http.ByteArrayContent;
         import com.google.api.client.http.HttpTransport;
         import com.google.api.client.json.gson.GsonFactory;
         import com.google.api.services.drive.Drive;
         import com.google.api.services.drive.DriveScopes;
+        import com.google.api.services.drive.model.File;
+        import com.google.api.services.drive.model.FileList;
+        import com.google.api.services.drive.model.ParentReference;
         //import com.google.gson.Gson;
 
         import java.io.IOException;
         import java.lang.reflect.InvocationTargetException;
         import java.lang.reflect.Method;
+        import java.nio.charset.Charset;
         import java.util.ArrayList;
         import java.util.Collection;
         import java.util.Collections;
@@ -704,8 +711,8 @@ public class MainActivity extends Activity implements SensorEventListener, LineR
                     }
                 } else {
                     // エラー処理
-                    Log.e(getTag(), "Google Login:miss");
-                    //addTextView(peerView, "アカウント名を取得失敗");
+                    Log.e(getTag(), "Google Login:miss!");
+                    addTextView(peerView, "Google Login:miss!");
                     activityResult = "";
                 }
                 peerGetID();
@@ -714,9 +721,10 @@ public class MainActivity extends Activity implements SensorEventListener, LineR
                 if ( resultCode == RESULT_OK ) {
                     Log.e(getTag(),  "REQUEST_AUTHORIZATION_FROM_DRIVE : Login success.");
 
-                    //addTextView(peerView, " : Login success.");
+                    addTextView(peerView, " : Login success.");
                     activityResult += " : Login success.";
-
+                    //ログイン後再度GoogleドライブにPeerIDを書き込みに行く
+                    new setDrivePeerIdTask().execute(mDrive, _id);
                 } else {
                     // エラー処理
                     Log.e(getTag(), "REQUEST_AUTHORIZATION_FROM_DRIVE : Login Miss!.");
@@ -890,7 +898,7 @@ public class MainActivity extends Activity implements SensorEventListener, LineR
                         GsonFactory factory = new GsonFactory();
                         mDrive = new Drive.Builder(transport, factory, mCredential).build();
                     }
-                    new setDrivePeerIdTask(peerView).execute(mDrive, _id, this);
+                    new setDrivePeerIdTask().execute(mDrive, _id);
 
                     //AsyncTask非同期処理後のコールバック機能
                     //https://qiita.com/a_nishimura/items/1548e02b96bebd0d43e4
@@ -1332,5 +1340,136 @@ public class MainActivity extends Activity implements SensorEventListener, LineR
         //final String tag = simpleClass + "# " + mthd + ": " + line;
         return simpleClass + "# " + mthd + ": " + line;
     }
+
+//グーグルのAPIを使うときに欠かせないGoogle OAuthの作り方と使い方 (3/3)  //http://www.atmarkit.co.jp/ait/articles/1509/15/news017_3.html
+// AsyncTask | Android Developers http://developer.android.com/intl/ja/reference/android/os/AsyncTask.html
+//AsyncTaskを使った非同期処理のきほん http://dev.classmethod.jp/smartphone/android/asynctask/
+//ジェネリクス<>の指定 doInBackgroundメソッドの引数の型, onProgressUpdateメソッドの引数の型, onPostExecuteメソッドの戻り値の型
+//doInBackgroundメソッドに複数の異なるデータ型の引数を渡したい場合は、Object型を指定してdoInBackground内でキャスト
+//  http://niudev.blogspot.jp/2012/02/blog-post.html
+
+    private class setDrivePeerIdTask extends AsyncTask<Object, String, Boolean> {
+        //private TextView textView;
+        static final String SKYWAYRC_DIR = "SkyWayRC";
+        static final String SKYWAY_ANDROID_ID = "SkyWayAndroid.id";
+
+        @Override
+        protected Boolean doInBackground(Object... params) {
+            String skyWayRC_FolderId = null;// 指定のタイトルのフォルダID を取得
+            Drive mDrive = (Drive) params[0];
+            String peerId = (String) params[1];
+            //context = (Context) params[2];
+            Log.e("doInBackground", mDrive.toString());
+            //Intent mIntent = null;
+            try {
+                // API呼び出し  // クエリ文字列をセット（以下は"'root' in parents MIMETYPEを指定）クエリ使わないとレスポンス遅い。ゴミ箱でないクエリ追加
+                Drive.Files.List request = mDrive.files().list().setQ("trashed = false and 'root' in parents and mimeType = 'application/vnd.google-apps.folder'");
+                List<File> mFolderList = new ArrayList<>();
+                //do..while文 http://www.javadrive.jp/start/for/index7.html
+                do {
+                    FileList files = request.execute();
+                    // 取得したFileリストを保持用のメンバーにセット
+                    mFolderList.addAll(files.getItems());
+                    // 全アイテムを取得するために繰り返し
+                    request.setPageToken(files.getNextPageToken());
+                } while (request.getPageToken() != null && request.getPageToken().length() > 0);
+
+                //拡張for文(for-each文) http://www.javadrive.jp/start/for/index8.html
+                for (File f : mFolderList) {
+                    //フォルダチェック getExplicitlyTrashed() ゴミ箱にあるのか調べるの追加
+                    if (SKYWAYRC_DIR.equals(f.getTitle()) && !f.getExplicitlyTrashed()) {
+                        skyWayRC_FolderId = f.getId();
+                        publishProgress("Exist a folder." + f.getTitle());
+                        Log.e("Exist a folder.", f.getTitle() + " MimeType: " + f.getMimeType() + " p: " + f.getParents());
+                        break;
+                    }
+                }
+                // タイトルのファイルの ID を取得
+                String fileIdOrNull = null;
+                if (skyWayRC_FolderId != null) {
+                    Log.e("skyWayRC_FolderId", skyWayRC_FolderId);
+                    //ID ファイルチェック
+                    // API呼び出し  // クエリ文字列をセット（以下はskyWayRC_FolderIdを指定）
+                    Drive.Files.List requestF = mDrive.files().list().setQ("'" + skyWayRC_FolderId + "' in parents");
+                    List<File> mFileListF = new ArrayList<>();
+                    //do..while文 http://www.javadrive.jp/start/for/index7.html
+                    do {
+                        FileList files = requestF.execute();
+                        // 取得したFileリストを保持用のメンバーにセット
+                        mFileListF.addAll(files.getItems());
+                        // 全アイテムを取得するために繰り返し
+                        requestF.setPageToken(files.getNextPageToken());
+                    } while (requestF.getPageToken() != null && requestF.getPageToken().length() > 0);
+                    for (File f : mFileListF) {
+                        if (SKYWAY_ANDROID_ID.equals(f.getTitle())) {
+                            fileIdOrNull = f.getId();
+                            publishProgress("Exist a file." + f.getTitle());
+                            break;
+                        }
+                    }
+                } else {
+                    //SKYWAYRC_DIR フォルダ作成 Creating a folder https://developers.google.com/drive/v2/web/folder#creating_a_folder
+                    File body = new File();
+                    body.setTitle(SKYWAYRC_DIR);
+                    body.setMimeType("application/vnd.google-apps.folder");
+                    Log.e("Creating a folder", SKYWAYRC_DIR);
+                    File f = mDrive.files().insert(body).execute();
+                    // folder ID.
+                    skyWayRC_FolderId = f.getId();
+                    publishProgress("Created a folder" + f.getTitle());
+                    Log.e("Created a folder", f.getTitle());
+                }
+
+                File body = new File();
+                body.setTitle(SKYWAY_ANDROID_ID);//fileContent.getName());
+                body.setMimeType("text/plain");
+                body.setDescription(peerId);
+                body.setParents(Collections.singletonList(new ParentReference().setId(skyWayRC_FolderId)));
+                //フォルダ指定  Java で要素がひとつだけのコレクションをつくる http://www.metareal.org/2007/10/07/java-singleton-collection/
+                //body.setParents(Collections.singletonList(new ParentReference().setId(skyWayRC_FolderId)));
+                ByteArrayContent content = new ByteArrayContent("text/plain", peerId.getBytes(Charset.forName("UTF-8")));
+                if (fileIdOrNull == null) {
+                    mDrive.files().insert(body, content).execute();
+                    publishProgress("insert!");
+                    Log.e("insert a file", SKYWAY_ANDROID_ID);
+                } else {
+                    mDrive.files().update(fileIdOrNull, body, content).execute();
+                    publishProgress("PeerID update!");
+                    Log.e("update a file", SKYWAY_ANDROID_ID);
+                }
+            //この例外は、ユーザーが認証していない場合に発生、つまり初回は必ず発生する例外です。つまり、例外の情報は重要ではなく、この例外から取得可能なIntentが重要になります。ここではフィールドに保存して、後で利用するようにしています。
+            } catch (UserRecoverableAuthIOException e) {
+                //mIntent = e.getIntent();
+                startActivityForResult(e.getIntent(), REQUEST_AUTHORIZATION_FROM_DRIVE);
+                Log.e("AuthIOException", e.getMessage(), e);
+                publishProgress("UserRecoverableAuthIOException");
+            } catch (IOException e) {
+                Log.e("doInBackground", e.getMessage(), e);
+                publishProgress("doInBackground IOException:" +  e.getMessage());
+            }
+            return true;
+        }
+
+//        @Override
+//        protected void onPostExecute(Intent mIntent) {
+//            if (mIntent != null) {
+//                startActivityForResult(mIntent, REQUEST_AUTHORIZATION_FROM_DRIVE);
+//                Log.e("onPostExecute", mIntent.toString());//"onPostExecute Done.");
+//            }
+//        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            //Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            //Android TextView : “Do not concatenate text displayed with setText” http://stackoverflow.com/questions/33164886/android-textview-do-not-concatenate-text-displayed-with-settext
+            //追加した行を一番上に表示させる。
+            String msg = values[0] + "\n";
+            String addMsg = msg + peerView.getText();
+            peerView.setText(addMsg);
+            //textView.append(msg);
+        }
+    }
+//AsyncTask非同期処理後のコールバック機能
+//https://qiita.com/a_nishimura/items/1548e02b96bebd0d43e4
 
 }
